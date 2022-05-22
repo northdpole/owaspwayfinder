683836f8a46d43b6c915e3b5a1a31d6eeb3c3ea6
spyros <morfeas3000@gmail.com>
2022-05-22 15:49:48 +0100

    add ports

--- main.py
+++ main.py
@@ -4,24 +4,30 @@ import graphviz as gz
 import yaml
 
 
-def cluster(root_graph:gz.Digraph,name:str,label:str, nodes,rfrom,rto):
+def cluster(root_graph:gz.Digraph,name:str,label:str, nodes):
     result = gz.Digraph(name=name)
     result.attr(label=label)
     result.attr(color='blue')
-    for i in range(rfrom,rto):
-        result.node(nodes[i])
-        result.edge(nodes[i-1],nodes[i])
-        prev_group=nodes[i]
-        for group,elements in steps[nodes[i]].items():
-            group_subgraph_name=f"cluster-{nodes[i]}-{group}"
+    for name,attrs in nodes.items():
+        prev_index = list(nodes.keys()).index(name) -1
+        prev_node = list(nodes.keys())[prev_index]
+        result.node(name)
+        result.edge(f"{prev_node}:{nodes[prev_node]['port_next']}", f"{name}:{attrs['port_previous']}")
+        prev_group=name
+        for group,elements in steps[name].items():
+            group_subgraph_name=f"cluster-{name}-{group}"
             group_subgraph = gz.Digraph(name=group_subgraph_name)
             group_subgraph.attr(rankdir="TB")
             group_subgraph.attr(label=group)
             group_subgraph.attr(color='blue')
             for project in elements:
-                group_subgraph.node(project['name'],href=project['href'],_attributes={"shape":"plaintext"})
-                result.edge(prev_group,group_subgraph_name,label=group,
-                            _attributes={"lhead":nodes[i],"ltail":group_subgraph_name})                
+                group_subgraph.node(project['name'],
+                                    href=project['href'],
+                                    _attributes={"shape":"plaintext"})
+                result.edge(prev_group,
+                            group_subgraph_name,
+                            label=group,
+                            _attributes={"lhead":name,"ltail":group_subgraph_name})                
             prev_group=group_subgraph_name
                 
             root_graph.subgraph(group_subgraph)
@@ -29,15 +35,15 @@ def cluster(root_graph:gz.Digraph,name:str,label:str, nodes,rfrom,rto):
     root_graph.subgraph(result)
     return root_graph
 
-default_nodes=["Requirements",
-               "Design",
-               "Implementation",
-               "Verification",
-               "Policy Gap Evaluation",
-               "Metrics",
-               "Training/Education",
-               "Culture Building and Process Maturing",
-               "Operation"]
+default_nodes={"Requirements":{"port_previous":"e","port_next":"w","port_external":"nw"},
+               "Design":{"port_previous":"s","port_next":"n","port_external":"nw"},
+               "Implementation":{"port_previous":"s","port_next":"n","port_external":"nw"},
+               "Verification":{"port_previous":"s","port_next":"n","port_external":"nw"},
+               "Policy Gap Evaluation":{"port_previous":"s","port_next":"e","port_external":"nw"},
+               "Metrics":{"port_previous":"w","port_next":"s","port_external":"nw"},
+               "Training/Education":{"port_previous":"n","port_next":"s","port_external":"nw"},
+               "Culture Building and Process Maturing":{"port_previous":"n","port_next":"s","port_external":"nw"},
+               "Operation":{"port_previous":"n","port_next":"w","port_external":"nw"}}
 dot = gz.Digraph(filename="/tmp/wayfinder.dot",strict=True)
 dot.attr(label="Application Security Wayfinder")
 dot.attr(overlap='false')
@@ -59,6 +65,6 @@ for dat in data:
         steps[s][dat["group"]].append(dat)
 
 
-dot = cluster(root_graph=dot,name="cluster-devops",label="The DEVOPs cycle",nodes=default_nodes,rfrom=0,rto=int(len(default_nodes)))
+dot = cluster(root_graph=dot,name="cluster-devops",label="The DEVOPs cycle",nodes=default_nodes)
 # print(dot)
 dot.render("/tmp/wayfinder.svg",view=True)
